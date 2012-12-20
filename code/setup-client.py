69ee020498520b2d64b075b346a95ad82cae8a5a
Alan haha <guyuhao088@gmail.com>
2012-12-20 15:30:37 +0800

    fix setup script for py2exe

--- code/setup-client.py
+++ code/setup-client.py
@@ -1,4 +1,4 @@
 #coding=utf-8  
 from distutils.core import setup  
 import py2exe  
-setup(windows=[{'script':'IM-haha-Client.py'}],options={'py2exe':{'includes':['sip','PyQt4']}})
+setup(windows=[{'script':'Client.py'}],options={'py2exe':{'includes':['sip','PyQt4']}})
