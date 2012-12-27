6b1dfa25a12a08ddcfdcd886b0c86aba03583b8a
Alan haha <guyuhao088@gmail.com>
2012-12-27 13:19:57 +0800

    add feature: set server IP

---
+++ code/FileServer/setup-fs.py
@@ -0,0 +1,17 @@
+#coding=utf-8  
+from distutils.core import setup  
+import py2exe
+
+setup(console=[{
+                "script": 'FileServer.py',
+               }],
+      options={"py2exe":{
+                         "optimize":2,
+                         #打包成一个文件，加快读取速度
+                         "compressed":1,
+                         "bundle_files":2,
+                         #少了个文件,不管它
+                         "dll_excludes":["MSVCP90.dll"],
+                         },
+               },
+      zipfile=None,)#一个文件
